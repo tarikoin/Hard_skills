2759e32084dc63d0cff5e9652af249e6b01254e9
tarikoin <98327680+tarikoin@users.noreply.github.com>
2022-04-21 22:12:13 +0300

    Update Stepik_MySQL.sql

--- Stepik_MySQL.sql
+++ Stepik_MySQL.sql
@@ -141,4 +141,12 @@ WHERE price - (
    ) <= 150
 ORDER BY price;
 
+/*Вывести информацию (автора, книгу и количество) о тех книгах, количество экземпляров которых в таблице book не дублируется.*/
+SELECT author, title, amount FROM book
+WHERE amount IN (
+       SELECT amount FROM book
+       GROUP BY amount
+       HAVING COUNT(*) = 1
+   );
+   
 
