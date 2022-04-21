7d4f2c4337fc956bbad6bf15ae81fc0fd7d352b9
tarikoin <98327680+tarikoin@users.noreply.github.com>
2022-04-21 13:46:01 +0300

    Update Stepik_MySQL.sql

--- Stepik_MySQL.sql
+++ Stepik_MySQL.sql
@@ -131,3 +131,14 @@ FROM book
 WHERE price <= (
         SELECT avg(price) FROM book)
 ORDER BY price DESC;
+
+/*Вывести информацию (автора, название и цену) о тех книгах, цены которых превышают минимальную цену книги на складе не более чем на 150 рублей */
+/*в отсортированном по возрастанию цены виде.*/
+SELECT author, title, price FROM book
+WHERE price - (
+       SELECT min(price) 
+       FROM book
+   ) <= 150
+ORDER BY price;
+
+
